eb5947cbf3cab3f86c764ee2d5d27372930d7d36
circulomedico <eribas@gmail.com>
2025-04-25 01:02:12 -0300

    Update firebaseConfig.ts

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyD2dr_tE2i8kkKsiSJYujlPIfnCTXp-S-4",
   authDomain: "circulomedico1234.firebaseapp.com",
   projectId: "circulomedico1234",
-  storageBucket: "circulomedico1234.appspot.com",
+  storageBucket: "circulomedico1234.firebaseapp.app",
   messagingSenderId: "292572112647",
   appId: "1:292572112647:web:e8b0e70d49c94750d907a2",
   measurementId: "G-26Q6QB985F"
