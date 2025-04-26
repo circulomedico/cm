dcf088a65b0e5be9af13dfcae79f7d1ba5969823
circulomedico <eribas@gmail.com>
2025-04-26 20:30:25 -0300

    Delete src/pages/medico/mapa.tsx

--- src/pages/medico/mapa.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function MapaProfissional() {
-  return (
-    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
-      <h1>Mapa Profissional Funcionando</h1>
-    </div>
-  );
-}
