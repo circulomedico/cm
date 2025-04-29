235bb68fc015cc19703fe3a7af8fa920c3997864
circulomedico <eribas@gmail.com>
2025-04-29 00:03:57 -0300

    Update mapa.tsx

--- src/pages/medico/mapa.tsx
+++ src/pages/medico/mapa.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { auth, db } from "../../firebase/firebaseConfig";
 import { onAuthStateChanged } from "firebase/auth";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { estadosBrasil, cidadesPorEstado } from "../../utils/cidadesBrasil";
 
 
