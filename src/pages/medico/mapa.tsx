c625a9a5c329edf422231b9b2aa1d3ba3caf2702
circulomedico <eribas@gmail.com>
2025-04-28 23:22:57 -0300

    Update mapa.tsx

--- src/pages/medico/mapa.tsx
+++ src/pages/medico/mapa.tsx
@@ -5,36 +5,6 @@ import { doc, getDoc } from "firebase/firestore";
 import { estadosBrasil, cidadesPorEstado } from "../../utils/cidadesBrasil";
 
 
-// Listagem de estados brasileiros
-const estadosBrasil = [
-  { sigla: "AC", nome: "Acre" },
-  { sigla: "AL", nome: "Alagoas" },
-  { sigla: "AP", nome: "Amapá" },
-  { sigla: "AM", nome: "Amazonas" },
-  { sigla: "BA", nome: "Bahia" },
-  { sigla: "CE", nome: "Ceará" },
-  { sigla: "DF", nome: "Distrito Federal" },
-  { sigla: "ES", nome: "Espírito Santo" },
-  { sigla: "GO", nome: "Goiás" },
-  { sigla: "MA", nome: "Maranhão" },
-  { sigla: "MT", nome: "Mato Grosso" },
-  { sigla: "MS", nome: "Mato Grosso do Sul" },
-  { sigla: "MG", nome: "Minas Gerais" },
-  { sigla: "PA", nome: "Pará" },
-  { sigla: "PB", nome: "Paraíba" },
-  { sigla: "PR", nome: "Paraná" },
-  { sigla: "PE", nome: "Pernambuco" },
-  { sigla: "PI", nome: "Piauí" },
-  { sigla: "RJ", nome: "Rio de Janeiro" },
-  { sigla: "RN", nome: "Rio Grande do Norte" },
-  { sigla: "RS", nome: "Rio Grande do Sul" },
-  { sigla: "RO", nome: "Rondônia" },
-  { sigla: "RR", nome: "Roraima" },
-  { sigla: "SC", nome: "Santa Catarina" },
-  { sigla: "SP", nome: "São Paulo" },
-  { sigla: "SE", nome: "Sergipe" },
-  { sigla: "TO", nome: "Tocantins" }
-];
 
 // Listagem de cidades para cada estado (iremos carregar de forma simplificada para exemplo)
 // AQUI nesta versão reduzida para envio no chat eu mostro apenas SP, RJ e MG como exemplo. 
